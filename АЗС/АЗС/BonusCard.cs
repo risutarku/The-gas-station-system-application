e076465d1089b20fcf24e4f035128fec2ed409ba
Балашов Антон <vity.esenin@mail.ru>
2023-10-09 18:30:25 +0700

    Добавить новый класс для бонусных карт

---
+++ АЗС/АЗС/BonusCard.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace АЗС
+{
+    internal class BonusCard
+    {
+        public int cardId
+        {
+            get { return cardId; } set { cardId = value; }
+        }
+
+        public int bonusAmount
+        {
+            get { return bonusAmount; } set { bonusAmount = value; }
+        }
+
+        public int totalPurchaseAmount
+        {
+            get { return totalPurchaseAmount; } set { totalPurchaseAmount = value; }
+        }
+
+        private BonusCard()
+        {
+            bonusAmount = 0;
+            totalPurchaseAmount = 0;
+        }
+
+
+
+    }
+}
